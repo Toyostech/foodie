6fbe944116954660a64f7f0296b84f843d2a8e34
SALAMI TOYOSI <106937039+Toyostech@users.noreply.github.com>
2024-11-13 23:08:31 +0100

    firt

---
+++ html and css/html/Resources/javascript/foodlive.js
@@ -0,0 +1,12 @@
+
+alert('please reload the page')
+console.log(alert)
+$(document).ready(function() {
+    $('h1').click(function() {
+        $(this).css('background-color','#ff0000')
+    })
+
+
+
+
+});
